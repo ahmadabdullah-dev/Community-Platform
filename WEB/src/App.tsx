0202e66ac83c353659915ebd8bbad06965c9595b
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-06-28 19:13:55 +0300

    Add project files.

---
+++ WEB/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Community Platform
+    </>
+  )
+}
+
+export default App
---
+++ WEB/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+export default defineConfig({
+  server:{
+    port:3000
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})

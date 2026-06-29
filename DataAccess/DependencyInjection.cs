9dfe4bb6e20815ab1d6177f95a725a030866a4bb
Ahmet Abdullah <ahmadabdullah.formal@gmail.com>
2026-06-30 02:27:57 +0300

    Create AppUser and AppRole. Create DataAccess and Business layers. Run first migration

---
+++ DataAccess/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+
+            }).AddRoles<AppRole>()
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
---
+++ DataAccess/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Entities.Identity;
+
+public class AppUser : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? Country { get; set; } 
+    public DateOnly? DateOfBirth { get; set; }
+}
